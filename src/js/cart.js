492592247cc86b16d8a0e9f18029e5b4c2aac518
fercuba96 <fercuba96@gmail.com>
2024-11-16 02:31:22 -0500

    step 2 finished of Individual activity

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,7 +1,11 @@
 import { getLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
-  const cartItems = getLocalStorage("so-cart");
+  const cartItems = getLocalStorage("so-cart")|| []; // Fallback to an empty array if cart is null
+  if (cartItems.length === 0) {
+    document.querySelector(".product-list").innerHTML = "<p>Your cart is empty.</p>";
+    return;
+  }
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
 }
