f8aebc978835bc9f4130de6f163ba47338de2a79
fercuba96 <fercuba96@gmail.com>
2024-12-02 18:33:34 -0500

    updated changes

--- src/js/ProductList.js
+++ src/js/ProductList.js
@@ -2,8 +2,8 @@ import { renderListWithTemplate } from "./utils.mjs";
 
 function productTemplate (product){
     return `<li class="product-card">
-      <a href="produc_pages/index.html?product=${product.Id}">
-      <img src="${product.Image}" alt="${product.Name}" class="product-image" />
+      <a href="/product_pages/index.html?product=${product.Id}">
+      <img src="${product.Images.PrimaryMedium}" alt="${product.Name}" class="product-image" />
       <h3 class="card_brand">${product.Brand.Name}</h3>
       <h2 class="card_name">${product.Name}</h2>
       <p class="product-card_price">Price: $${product.FinalPrice}</p></a>
@@ -21,7 +21,12 @@ constructor(category, dataSource, listElement){
 filterProducts(list) {return list.slice(0, 4);}
 
 async init(){
-    const list = await this.dataSource.getData();
+   document.title = `Top Products: ${this.category.charAt(0).toUpperCase() + this.category.slice(1)}`;
+  const categoryNameElement = document.getElementById("category-name");
+  if (categoryNameElement) {
+    categoryNameElement.textContent = `Top products: ${this.category.charAt(0).toUpperCase() + this.category.slice(1)}`;
+  }    
+  const list = await this.dataSource.getData(this.category);
     const filteredProducts = this.filterProducts(list);
     this.renderList(filteredProducts); 
   } catch (error) {
--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,14 +1,14 @@
 import ProductData from "./ProductData.mjs";
 
-import ProductListing from "./ProductList.mjs";
+import ProductListing from "./ProductList.js";
 
 import {loadHeaderFooter, getParams} from "./utils.mjs";
+
+loadHeaderFooter();
 
 const category = getParams("category");
 const dataSource = new ProductData();
 const element = document.querySelector(".product-list");
 const productListing = new ProductListing(category,dataSource,element);
-
-loadHeaderFooter();
 
 productListing.init();
